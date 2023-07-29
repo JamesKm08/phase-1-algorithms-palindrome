132ee5b908e1184608b1e9c4fc76e88d2c5e3d01
JamesKm08 <66277467+JamesKm08@users.noreply.github.com>
2023-07-29 20:12:51 +0300

    Lab Complete

--- index.js
+++ index.js
@@ -8,17 +8,21 @@ isPalindrome = (word) => {
 
 /* 
   Add your pseudocode here
-Change the word passed to lowercase
-Reverse the word to compare it with original word
-Return the comparison of the two words to check if it's a palindrome
+
+  reverse the input string
+if the reversed string is the same as the input
+  return true
+else
+  return false
 */
 
 /*
   Add written explanation of your solution here
+
   First change the word into lowercase to ensure uniformity
   Then reverse the word but first spliting then reversing then joining so that the original word is reversed
   Then return the reversed word if strictly same with the original word
-*/
+  
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
